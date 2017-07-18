fa8497107da6d089eacc1254b6a35a95a8149cb4
Bill Gatliff <bgat@billgatliff.com>
2017-07-18 22:33:01 +0000

    We don't need to specify the namespace

--- include/px4/linux-regio.h
+++ include/px4/linux-regio.h
@@ -1,13 +1,13 @@
 #if !defined(__LINUX_REGIO__INCLUDED__)
 #define __LINUX_REGIO__INCLUDED__
 
-class i2c_regio : public px4::regio {
+class i2c_regio : public regio {
 public:
 	i2c_regio(int bus, int addr);
 	~i2c_regio();
 	bool is_ok() { return fd != -1 ? true : false; }
-	bool read_reg(const px4::reg_t &reg, int &val);
-	bool write_reg(const px4::reg_t &reg, int val);
+	bool read_reg(const reg_t &reg, int &val);
+	bool write_reg(const reg_t &reg, int val);
 
 private:
 	i2c_regio(/* i2c clients need addresses specified */ ) {}
@@ -16,7 +16,7 @@ private:
 
 
 // TODO: Linux userspace spidev register accessor
-class spi_regio : public px4::regio {
+class spi_regio : public regio {
 public:
 	// @id: client identification string, in the form "spi:<bus>.<slave>"
 	spi_regio(const char *id) {
@@ -26,15 +26,15 @@ public:
 	~spi_regio() {}
 
 	// TODO: implement these
-	bool read_reg(const px4::reg_t &reg, int &val) { return false; }
-	bool write_reg(const px4::reg_t &reg, int val) { return false; }
+	bool read_reg(const reg_t &reg, int &val) { return false; }
+	bool write_reg(const reg_t &reg, int val) { return false; }
 private:
 	spi_regio(/* spi clients need addresses specified */ ) {}
 	int fd;
 };
 
 // TODO: Linux mmio accessor
-class mem_regio : public px4::regio {
+class mem_regio : public regio {
 public:
 	// @id: client identification string, in the form "mem:<len>@<physaddr>"
 	mem_regio(const char *id) {
@@ -44,12 +44,11 @@ public:
 	~mem_regio() {}
 
 	// TODO: implement these
-	bool read_reg(const px4::reg_t &reg, int &val) { return false; }
-	bool write_reg(const px4::reg_t &reg, int val) { return false; }
+	bool read_reg(const reg_t &reg, int &val) { return false; }
+	bool write_reg(const reg_t &reg, int val) { return false; }
 private:
 	mem_regio(/* mem clients need addresses specified */ ) {}
 	void *mem;
 };
-
 
 #endif
